2ba61da8aaa446bc116cb68e8f1bfc81e4902c4e
Katie Leonard <k80.leonard@gmail.com>
2013-12-14 11:09:00 -0800

    Adds notes to collection.

--- collections/posts.js
+++ collections/posts.js
@@ -2,3 +2,6 @@
 // to be available to the whole app
 // the var keyword would scope it to the current file
 Posts = new Meteor.Collection('posts');
+
+// Collection acts as an API into the Mongo database
+// Allows you to write "Posts.insert()"
