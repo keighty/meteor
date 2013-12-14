3d5bc58285af198e694ca6566f9c5146f7dad5e5
Katie Leonard <k80.leonard@gmail.com>
2013-12-14 11:08:26 -0800

    Replace static data with database call.

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,31 +1,6 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    author: 'Sacha Greif',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    author: 'Tom Coleman',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'The Meteor Book',
-    author: 'Tom Coleman',
-    url: 'http://themeteorbook.com'
-  },
-  {
-    title: 'A Whole New World',
-    author: 'Me',
-    url: '#'
-  },
-  {
-    title: 'FooBar and the foobars',
-    author: 'Me',
-    url: '#'
-  }
-];
 // this adds the posts helpers to the meteor helpers
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
